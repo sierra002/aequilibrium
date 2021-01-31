44081188dfe523f908a0446512ab7ad46aba6879
Sebastian Sierra <ssierrao@psl.com.co>
2021-01-30 20:28:05 -0500

    Adding unit test for the transformer vs

--- src/app/shared/services/transformer.service.spec.ts
+++ src/app/shared/services/transformer.service.spec.ts
@@ -1,7 +1,9 @@
 import {TransformerService} from "./transformer.service";
+import {Transformer} from "../models/transformer";
 
 describe('Transformer Service', () => {
-  let service;
+  let service: TransformerService;
+  let transformerA, transformerB: Transformer;
 
   beforeEach(() => {
     service = new TransformerService();
@@ -9,5 +11,50 @@ describe('Transformer Service', () => {
 
   it('should exist', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('transformerVs', () => {
+    beforeEach(() => {
+      transformerA = new Transformer('sebas', 4, 4, 4, 4, 4, 4, 4, 4);
+      transformerB = new Transformer('sebas2', 1, 4, 4, 4, 4, 0, 4, 4);
+    });
+    it('a fighter has more than 4 courage points and 3 more strength he win', () => {
+      expect(service.transformerVs(transformerA, transformerB)).toEqual(true)
+    });
+    it('should win if one has more overall rating', () => {
+      transformerB.strength = 5;
+      transformerB.courage = 4;
+      expect(service.transformerVs(transformerA, transformerB)).toEqual(false);
+    });
+    it('should win if has 3 more points of skill', () => {
+      transformerB.skill = 7;
+      transformerB.strength = 4;
+      transformerB.courage = 4;
+      expect(service.transformerVs(transformerA, transformerB)).toEqual(false);
+    });
+    it('should set a tie (null) if both transformers have the same stats', () => {
+      transformerB.strength = 4;
+      transformerB.courage = 4;
+      expect(service.transformerVs(transformerA, transformerB)).toEqual(null);
+    });
+    describe('special rules', () => {
+      let transformerC, transformerD;
+      beforeEach(() => {
+        transformerC = new Transformer('Optimus Prime', 0, 0, 0, 0, 0, 0, 0, 0);
+        transformerD = new Transformer('predaking', 0, 0, 0, 0, 0, 0, 0, 0);
+      });
+      it('should win if name is optimus prime or predaking', () => {
+        expect(service.transformerVs(transformerC, transformerA)).toEqual(true);
+        expect(service.transformerVs(transformerD, transformerB)).toEqual(true);
+      });
+
+      it('should throw an exception if 2 super transformers fight', () => {
+        try {
+          service.transformerVs(transformerC, transformerD)
+        } catch (e) {
+          expect(e.message).toEqual("All competitors destroyed");
+        }
+      });
+    });
   });
 });
